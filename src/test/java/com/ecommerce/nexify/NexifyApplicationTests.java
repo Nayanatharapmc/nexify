b04841b0b7e082dc5c386146d0d85355376af6ab
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2026-03-21 08:13:01 +0530

    Initial commit

---
+++ src/test/java/com/ecommerce/nexify/NexifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.nexify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
