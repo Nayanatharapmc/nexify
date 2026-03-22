b6cbb33fce87021ac92bc4be70344ddbd3764560
Chethmi Nayanathara <chethmi.21@cse.mrt.ac.lk>
2026-03-22 08:38:32 +0530

    Add Lombok for code clarity and readability

--- src/main/java/com/ecommerce/nexify/model/Category.java
+++ src/main/java/com/ecommerce/nexify/model/Category.java
@@ -1,37 +1,18 @@
 package com.ecommerce.nexify.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity (name = "Categories") // Converts the java class to a JPA entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id  // Defines the primary key
-//    @GeneratedValue(strategy = GenerationType.IDENTITY) // Manages the generated value of the primary key
+    // @GeneratedValue(strategy = GenerationType.IDENTITY) // Manages the generated value of the primary key
     private String categoryId;
     private String categoryName;
-
-    public Category(){}
-
-    public Category(String categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
